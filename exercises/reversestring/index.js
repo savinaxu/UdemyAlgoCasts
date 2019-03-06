cf3f971c191a3d52d0dc8d73a615a9b8f8ebc853
savinaxu <jia_shijie@163.com>
2019-03-06 13:56:38 -0800

    reversestring done.

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,33 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+
+// solution 1
+// function reverse(str) {
+//     let result = ""
+//     for (let i = str.length-1; i >= 0; i--) {
+//         result += str[i]
+//     }
+//     return result
+// }
+
+//solution 2
+// function reverse(str) {
+//     return str.split("").reverse().join("")
+// }
+
+//solution 3
+// function reverse(str) {
+//     let result = ""
+//     for (let char of str) {
+//         result = char + result
+//     }
+//     return result
+// }
+
+//solution 4
+function reverse(str) {
+    return str.split("").reduce((rev, char) => char + rev, "")
+}
 
 module.exports = reverse;
