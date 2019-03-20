de2547aa4c7f782d9f5b34bdd84afa3174da684a
savinaxu <jia_shijie@163.com>
2019-03-19 21:27:47 -0700

    one more task.

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,40 +15,97 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
+//solution2
 function matrix(n) {
+    let result = makeMartix(n)
     let columnStart = 0,
         columnEnd = n-1,
         rowStart = 0,
         rowEnd = n-1
-    
-    let result = makeMartix(n),
         count = 1
 
-    while( count <= n*n ) {
-        for (let j = columnStart; j <= columnEnd; j++) {
-            result[rowStart][j] = count
-            count++
+    function recursionMatrix(count, currentI, currentJ, nextI, nextJ) {
+        
+        if (count > n * n) {
+            return
         }
-        rowStart++
-        for (let i = rowStart; i <= rowEnd; i++) {
-            result[i][columnEnd] = count
+
+        if (nextJ <= columnEnd) {
+            result[currentI][currentJ] = count
             count++
+            return recursionMatrix(count, currentI, currentJ+1, nextI, nextJ+1)
         }
-        columnEnd--
-        for (let m = columnEnd; m >= columnStart; m--) {
-            result[rowEnd][m] = count
+
+        if (nextJ > columnEnd) {
+            result[currentI][currentJ] = count
             count++
+            return recursionMatrix(count, currentI+1, currentJ, nextI, nextJ)
         }
-        rowEnd--
-        for (let n = rowEnd; n >= rowStart; n--) {
-            result[n][columnStart] = count
+
+        if (currentJ === columnEnd && currentI === rowStart) {
+            result[currentI][currentJ] = count
             count++
+            return recursionMatrix(count, currentI+1, currentJ)
         }
-        columnStart++
+
+        if (currentI < rowEnd) {
+            result[currentI][currentJ] = count
+            count++
+            return recursionMatrix(count, currentI+1, currentJ)
+        }
+
+        if ()
+        
+
+
     }
 
-    return result
+    function checkRange(a, b) {
+        if (a >= 0 && a < n-1 && b >= 0 && b < n-1 && result[a][b] === 0) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+
 }
+
+// solution1
+// function matrix(n) {
+//     let columnStart = 0,
+//         columnEnd = n-1,
+//         rowStart = 0,
+//         rowEnd = n-1
+    
+//     let result = makeMartix(n),
+//         count = 1
+
+//     while( count <= n*n ) {
+//         for (let j = columnStart; j <= columnEnd; j++) {
+//             result[rowStart][j] = count
+//             count++
+//         }
+//         rowStart++
+//         for (let i = rowStart; i <= rowEnd; i++) {
+//             result[i][columnEnd] = count
+//             count++
+//         }
+//         columnEnd--
+//         for (let m = columnEnd; m >= columnStart; m--) {
+//             result[rowEnd][m] = count
+//             count++
+//         }
+//         rowEnd--
+//         for (let n = rowEnd; n >= rowStart; n--) {
+//             result[n][columnStart] = count
+//             count++
+//         }
+//         columnStart++
+//     }
+
+//     return result
+// }
 
 const makeMartix = n => {
     let m = [];
